be1b91b26aaca3c287f1236193fb29b18ab368c8
Dave Winer <dave.winer@gmail.com>
2019-09-06 10:48:23 -0400

    v0.4.2

--- persists.js
+++ persists.js
@@ -1,28 +1,55 @@
-module.exports = makePersistentProxy;
+var myProductName = "persists", myVersion = "0.4.2";  
+
+/*  The MIT License (MIT)
+	Copyright (c) 2014-2019 Dave Winer
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	*/
+
+module.exports = createSharedObject;
 
 const utils = require ("daveutils");
 const fs = require ("fs");
 
-function makePersistentProxy (nameobject, obj, callback) {
+function createSharedObject (nameobject, obj, options, callback) {
 	var fname = nameobject + ".json";
 	var flchanged = false;
-	var theTarget;
-	
+	var theSharedObject;
+	if (options === undefined) { //options are optional ;-)
+		options = {
+			flLogToConsole: true
+			};
+		}
 	function saveIfChanged () {
 		if (flchanged) {
 			flchanged = false;
-			fs.writeFile (fname, utils.jsonStringify (theTarget), function (err) {
+			fs.writeFile (fname, utils.jsonStringify (theSharedObject), function (err) {
 				});
 			}
 		}
-	function get (target, name) {
-		return (target [name]);
-		}
-	function set (target, name, value) {
-		console.log ("name == " + name + ", value == " + value);
-		target [name] = value;
+	function set (obj, name, value) {
+		if (options.flLogToConsole) {
+			console.log ("name == " + name + ", value == " + value);
+			}
+		obj [name] = value;
 		flchanged = true;
-		theTarget = target;
+		theSharedObject = obj; 
 		}
 	fs.readFile (fname, function (err, data) {
 		if (!err) {
@@ -37,6 +64,6 @@ function makePersistentProxy (nameobject, obj, callback) {
 				}
 			}
 		setInterval (saveIfChanged, 1000);
-		callback (new Proxy (obj, {get, set}));
+		callback (new Proxy (obj, {set}));
 		});
 	}
--- readme.md
+++ readme.md
@@ -1,4 +1,33 @@
-### This is the persists package
+## The persists package
 
-Docs to come soon.
+Persistence for JavaScript objects in Node. 
+
+### How to use
+
+Here's an app that adds one to a counter every second. If you stop it and restart, counter will start with its last value.
+
+```
+
+const persists = require ("../persists.js");
+const inititalstats = {
+	counter: 0
+	};
+persists ("stats", inititalstats, undefined, function (stats) {
+	setInterval (function () {
+		stats.counter++;
+		}, 1000);
+	});
+
+
+```
+
+### Changes
+
+9/6/19 by DW
+
+Don't need a <i>get</i> handler, removed it. 
+
+Made the console message on setting optional, defaults true.
+
+Added an options param to persists, so we can pass other options in as-needed.
 
